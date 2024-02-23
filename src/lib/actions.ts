1c90afaa816083bcce6e423a9d97380d612b1e27
Emilio Sosa <emiliossosa85@gmail.com>
2024-02-22 23:13:55 -0400

    Disable SQL

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,34 +4,34 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
  
-export async function createInvoice(formData: FormData) {
-  // Validate form using Zod
-  const validatedFields = {
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  };
+// export async function createInvoice(formData: FormData) {
+//   // Validate form using Zod
+//   const validatedFields = {
+//     customerId: formData.get('customerId'),
+//     amount: formData.get('amount'),
+//     status: formData.get('status'),
+//   };
 
  
-  // Prepare data for insertion into the database
-  //const { customerId, amount, status } = validatedFields.data;
-  //const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
+//   // Prepare data for insertion into the database
+//   //const { customerId, amount, status } = validatedFields.data;
+//   //const amountInCents = amount * 100;
+//   const date = new Date().toISOString().split('T')[0];
  
-  // Insert data into the database
-  try {
-    await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
-  } catch (error) {
-    // If a database error occurs, return a more specific error.
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
-  }
+//   // Insert data into the database
+//   try {
+//     await sql`
+//       INSERT INTO invoices (customer_id, amount, status, date)
+//       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+//     `;
+//   } catch (error) {
+//     // If a database error occurs, return a more specific error.
+//     return {
+//       message: 'Database Error: Failed to Create Invoice.',
+//     };
+//   }
  
-  // Revalidate the cache for the invoices page and redirect the user.
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
-}
+//   // Revalidate the cache for the invoices page and redirect the user.
+//   revalidatePath('/dashboard/invoices');
+//   redirect('/dashboard/invoices');
+// }
